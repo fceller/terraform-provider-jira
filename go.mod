dd684199213fdb2fb080ca8a5a8b3b0dd69c6be0
Frank Celler <frank@arangodb.com>
2022-02-15 16:43:20 +0100

    update release script

--- go.mod
+++ go.mod
@@ -6,8 +6,10 @@ require (
 	github.com/andygrunwald/go-jira v1.13.0
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/hashicorp/go-version v1.4.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/hashicorp/terraform-plugin-sdk v1.17.2
+	github.com/mitchellh/gox v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/trivago/tgo v1.0.7
 )
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,47 +1,46 @@
 #!/bin/bash
 
 # define architecture we want to build
-XC_ARCH=${XC_ARCH:-"386 amd64 arm"}
-XC_OS=${XC_OS:-linux darwin windows freebsd openbsd solaris}
-XC_EXCLUDE_OSARCH="!darwin/arm !darwin/386"
+ARCH=${ARCH:-"amd64 arm64"}
+OS=${OS:-linux darwin}
+TAG=${TAG:-0.0.0}
 
 # clean up
-echo "-> running clean up...."
-rm -rf output/*
-rm -rf artifacts/*
-
-
-if ! which gox > /dev/null; then
-    echo "-> installing gox..."
-    go get -u github.com/mitchellh/gox
-fi
+echo "Running clean up..."
+rm -rf output
+rm -rf artifacts
 
 # build
 # we want to build statically linked binaries
 export CGO_ENABLED=0
-echo "-> building..."
-gox \
-    -os="${XC_OS}" \
-    -arch="${XC_ARCH}" \
-    -osarch="${XC_EXCLUDE_OSARCH}" \
-    -output "output/{{.OS}}_{{.Arch}}/terraform-provider-jira" \
-    .
+echo -n "Building... "
+for os in ${OS}; do
+    for arch in ${ARCH}; do
+        echo -n "${os}_${arch} "
+        env GOOS=$os GOARCH=$arch go build -o "output/terraform-provider-jira_${TAG}_${os}_${arch}/terraform-provider-jira_$TAG"
+        cp README.md output/terraform-provider-jira_${TAG}_${os}_${arch}
+        cp LICENSE output/terraform-provider-jira_${TAG}_${os}_${arch}
+    done
+done
+echo
 
 # Zip and copy to the dist dir
-echo ""
-echo "Packaging..."
+echo -n "Packaging... "
 mkdir artifacts
+
 for PLATFORM in $(find ./output -mindepth 1 -maxdepth 1 -type d); do
     OSARCH=$(basename ${PLATFORM})
-    echo "--> ${OSARCH}"
+    echo -n "${OSARCH} "
 
-    pushd $PLATFORM >/dev/null 2>&1
-    zip ../../artifacts/terraform-provider-jira_${OSARCH}.zip ./*
+    pushd output >/dev/null 2>&1
+    zip ../artifacts/${OSARCH}.zip ${OSARCH}/*
+    popd >/dev/null 2>&1
+
+    pushd artifacts >/dev/null 2>&1
+    shasum -a 256 ${OSARCH}.zip >> terraform-provider-jira_${TAG}_SHA256SUMS
     popd >/dev/null 2>&1
 done
 
-echo ""
-echo ""
-echo "-----------------------------------"
-echo "Output:"
-ls -alh output/
+pushd artifacts >/dev/null 2>&1
+gpg --detach-sign terraform-provider-jira_${TAG}_SHA256SUMS 
+popd >/dev/null 2>&1
